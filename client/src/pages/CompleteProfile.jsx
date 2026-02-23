1f404933ed0e1590aac77ef350508d558cee9fb8
vaishnavi4049 <vaishnaviadhav777@gmail.com>
2026-02-23 14:06:14 +0530

    Updated API URL

--- client/src/pages/CompleteProfile.jsx
+++ client/src/pages/CompleteProfile.jsx
@@ -49,7 +49,7 @@ const CompleteProfile = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/users/complete-profile",
+        "https://backend.onrender.com/api/users/complete-profile",
         formData,
         { withCredentials: true }
       );
